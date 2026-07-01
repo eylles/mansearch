222dff48b882eb77bdf066310cfe383e21d26621
eylles <ed.ylles1997@gmail.com>
2026-07-01 13:23:28 -0600

    Improve fzfman

--- fzfman.sh
+++ fzfman.sh
@@ -4,6 +4,9 @@ if [ -n "$FZF_PREVIEW_COLUMNS" ]; then
     export MANWIDTH="$FZF_PREVIEW_COLUMNS"
 fi
 
-export MAN_KEEP_FORMATTING=1
+man_bin="/usr/bin/man"
 
-exec man "$@" 2>/dev/null
+export MAN_KEEP_FORMATTING=1
+export MANPAGER=/usr/bin/cat
+
+exec $man_bin "$@" 2>/dev/null
